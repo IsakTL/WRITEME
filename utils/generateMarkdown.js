81c015937fbc52cc16587d1cb79f4f37f30c585b
Isak <isaktlarsson99@gmail.com>
2024-10-24 17:49:38 -0500

    Added comments, fixed license reference

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 //  This returns a license badge based on which license is selected
 function renderLicenseBadge() {
   let badgeLink;
-  switch(data.license){
+  switch (data.license) {
     case 'MIT':
       badgeLink = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
       break;
@@ -19,7 +19,7 @@ function renderLicenseBadge() {
 // If there is no license, return an empty string
 function renderLicenseLink() {
   let licenseLink;
-  switch(data.license){
+  switch (data.license) {
     case 'MIT':
       licenseLink = '[MIT License](https://spdx.org/licenses/MIT.html)';
       break;
@@ -35,6 +35,38 @@ function renderLicenseLink() {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  function renderLicenseBadge() {
+    let badgeLink;
+    switch (data.license) {
+      case 'MIT':
+        badgeLink = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        break;
+      case 'GNU':
+        badgeLink = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+        break;
+      case 'Apache':
+        badgeLink = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        break;
+    }
+    return badgeLink;
+  }
+
+  function renderLicenseLink() {
+    let licenseLink;
+    switch (data.license) {
+      case 'MIT':
+        licenseLink = '[MIT License](https://spdx.org/licenses/MIT.html)';
+        break;
+      case 'GNU':
+        licenseLink = '[GNU General Public License version 3.0 or later](https://spdx.org/licenses/GPL-3.0-or-later.html)';
+        break;
+      case 'Apache':
+        licenseLink = '[Apache License version 2.0](https://www.apache.org/licenses/LICENSE-2.0)';
+        break;
+    }
+    return licenseLink;
+  }
+
   return `# ${data.title}
 ${renderLicenseBadge()}
 ${data.desc}
